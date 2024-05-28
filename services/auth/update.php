740619209147432c09d07318f8d707acbaf4045f
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-05-28 10:26:21 -0300

    Create update.php

---
+++ services/auth/update.php
@@ -0,0 +1,14 @@
+<?php
+
+// rota executor query
+// require_once('');
+
+function password_reset($password, $confirm_password){
+
+    //coloca a data e hora do dia
+    $agora = date('Y-m-d H:i:s');
+    $dt_updated_at = $agora;
+
+    // $sql1 = "UPDATE tabela SET coluna = 'valor', dt_updated_at = '$dt_updated_at' WHERE id = $id";
+    // executor($sql1);
+}
